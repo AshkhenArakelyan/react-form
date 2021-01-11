eef35d1816a7203d83bfaa8d45262752d1b1a1ba
AshkhenArakelyan <ashkhenarakelyank@gmail.com>
2021-01-11 15:47:58 +0400

    initial layout

--- src/components/InputPassword/InputPassword.js
+++ src/components/InputPassword/InputPassword.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
+
 
 const InputPassword = ({password, handleChangeInput}) => {
     return (
         <input type="password" placeholder="Password" name='password' value={password}  onChange={(event) => handleChangeInput(event)}/>
     );
 }
+
 
 export default InputPassword
